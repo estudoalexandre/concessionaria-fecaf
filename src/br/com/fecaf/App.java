0739146287e49c8393ad1fcc280cd0b4f55dcaa9
Alexandre Oliveira <alexandresousaa70@gmail.com>
2024-12-23 18:40:03 -0300

    First commit models

--- src/br/com/fecaf/App.java
+++ src/br/com/fecaf/App.java
@@ -1,12 +1,13 @@
 package br.com.fecaf;
 
+import br.com.fecaf.model.Carro;
 import br.com.fecaf.model.Moto;
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
+        Carro carro = new Carro();
 
     }
 }
